7571e6ca48314faa4a716a3392d580ddcf9d6c8a
Changyi SONG <pqz346292062@gmail.com>
2017-11-20 21:35:26 +0100

    Add files via upload

--- 3_chatbot_pythonAPI/static/main4.js
+++ 3_chatbot_pythonAPI/static/main4.js
@@ -18,9 +18,7 @@ $(document).ready(function() {
       data : { 'first': valueOne,'second': '' },
       success: function(results) {
         $('#results').html('<b>User :</b>' + valueOne + '<br>' + '<b>ChatbotVS :</b>' + results)
-          //$('#results').html('<a href="'+results.items[randNum].html_url+'">'+results.items[randNum].login+
-          //    '</a><br><img src="'+results.items[randNum].avatar_url+'" class="avatar">')
-          //$('input').val('')        
+          //return the specifique response format     
       },
       error: function(error) {
         console.log(error)
